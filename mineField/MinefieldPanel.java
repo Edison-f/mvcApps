fe983538223e6780cc491022115e8ea48310fb91
Steven Bui <kpopjellybean@gmail.com>
2023-03-16 00:33:02 -0700

    Implemented MinefieldFactory and MinefieldPanel MoveCommand mostly implemented, just need to work on model first before able to make movement.

--- mineField/MinefieldPanel.java
+++ mineField/MinefieldPanel.java
@@ -11,9 +11,44 @@ import java.beans.PropertyChangeListener;
 // TODO trim down imports if they're unnecessary, copied them from other code
 
 public class MinefieldPanel extends AppPanel {
+    private JButton NW;
+    private JButton N;
+    private JButton NE;
+    private JButton W;
+    private JButton E;
+    private JButton SW;
+    private JButton S;
+    private JButton SE;
     public MinefieldPanel(AppFactory factory) {
         super(factory);
-        // TODO implement constructor
+        NW = new JButton("NW");
+        N = new JButton("N");
+        NE = new JButton("NE");
+        W = new JButton("W");
+        E = new JButton("E");
+        SW = new JButton("SW");
+        S = new JButton("S");
+        SE = new JButton("SE");
+
+        NW.addActionListener(this);
+        N.addActionListener(this);
+        NE.addActionListener(this);
+        W.addActionListener(this);
+        E.addActionListener(this);
+        SW.addActionListener(this);
+        S.addActionListener(this);
+        SE.addActionListener(this);
+
+        controlPanel.add(NW);
+        controlPanel.add(N);
+        controlPanel.add(NE);
+        controlPanel.add(W);
+        controlPanel.add(E);
+        controlPanel.add(SW);
+        controlPanel.add(S);
+        controlPanel.add(SE);
+
+        controlPanel.setBackground(Color.WHITE);
     }
 
     @Override
--- mineField/MoveCommand.java
+++ mineField/MoveCommand.java
@@ -1,15 +1,27 @@
 package mineField;
 
 import mvc.*;
+import stopLight.Stoplight;
 
 public class MoveCommand extends Command {
-
     Heading heading; // TODO find a way to use the heading here?
-    public MoveCommand(Model model) {
+    public MoveCommand(Model model, Heading heading) {
         super(model);
+        this.heading = heading;
     }
 
     public void execute() throws Exception {
+        if (!(model instanceof Minefield)) {
+            throw new Exception("Model must instantiate Minefield");
+        }
+        Minefield field = (Minefield)model;
+
         // TODO implement MoveCommand's execute, make it throw exceptions if it isn't allowed?
+        switch(heading) {
+            case NORTH: {
+                // waiting to implement the model first and then we can work on moving?
+            }
+        }
+
     }
 }
